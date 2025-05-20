aafce9203275890819145d50e4167b3cc1aa7d3e
ananthu <heyananthulal@gmail.com>
2025-05-20 17:41:05 +0530

    total experience field added

--- app/client/_components/Ourteam.jsx
+++ app/client/_components/Ourteam.jsx
@@ -95,10 +95,10 @@ function Ourteam() {
 
                             {/* Header with Initial Avatar */}
                             <div className="flex flex-col items-center mb-6 space-y-2">
-                                <div className="w-20 h-20 rounded-full bg-gradient-to-r from-indigo-300 via-purple-300 to-pink-300 flex items-center justify-center text-3xl font-bold text-white shadow-inner">
+                                {/* <div className="w-20 h-20 rounded-full bg-gradient-to-r from-indigo-300 via-purple-300 to-pink-300 flex items-center justify-center text-3xl font-bold text-white shadow-inner">
                                     {selectedMember.name?.charAt(0)}
-                                </div>
-                                <h2 className="text-2xl font-bold text-gray-900">{selectedMember.name}</h2>
+                                </div> */}
+                                <h2 className="text-4xl font-bold text-gray-900">{selectedMember.name}</h2>
                                 <p className="text-sm text-gray-600">{selectedMember.position}</p>
                                 {selectedMember.totalexperience && (
                                     <p className="text-sm text-gray-600">
